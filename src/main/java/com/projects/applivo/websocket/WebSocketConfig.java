66071458376669f16961c5448ef881f7382dad20
Ashish Sharma <ashy07sharma@gmail.com>
2026-07-18 19:37:32 +0530

    bug fixes

--- src/main/java/com/projects/applivo/websocket/WebSocketConfig.java
+++ src/main/java/com/projects/applivo/websocket/WebSocketConfig.java
@@ -7,6 +7,7 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -31,5 +32,11 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/topic");
         registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void configureWebSocketTransport(WebSocketTransportRegistration registration) {
+        registration.setSendBufferSizeLimit(5 * 1024 * 1024);
+        registration.setSendTimeLimit(20000);
     }
 }
